858f4b4bd14f590b8b3b0ef692799992fceeaee0
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-30 14:24:10 +0100

    Updated port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const HTTP = require('http');
 const URL = require('url');
-const FS = require('fs');
+const fs = require('fs');
+const path = require('path');
 
 HTTP.createServer(async (request, response) => {	
 	const url = URL.parse(request.url, true);
 	console.log(url);
 
-	FS.readFile(`assets${url.path}`, (err, buffer) => {
+	fs.readFile(`assets${url.path}`, (err, buffer) => {
 		// if (!err) response.write(buffer);
 		// else {
 		// 	if (err.code === 'ENOENT') response.writeHead(404, 'File not found');
@@ -15,4 +16,4 @@ HTTP.createServer(async (request, response) => {
 		response.end();
 	});
 
-}).listen(8080);
+}).listen(process.env.PORT || 5000);
