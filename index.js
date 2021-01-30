e87a963454272fe2ed5d6cba3e32055536dfd59c
Dimitri-chln <dimitrichaline4@gmail.com>
2021-01-30 14:30:13 +0100

    .

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const HTTP = require('http');
+const http = require('http');
 const URL = require('url');
 const fs = require('fs');
 const path = require('path');
 
-HTTP.createServer(async (request, response) => {	
+http.createServer(async (request, response) => {
 	const url = URL.parse(request.url, true);
-	console.log(url);
 
-	fs.readFile(`assets${url.path}`, (err, buffer) => {
-		// if (!err) response.write(buffer);
-		// else {
-		// 	if (err.code === 'ENOENT') response.writeHead(404, 'File not found');
-		// }
+	fs.readFile(url.path, (err, buffer) => {
+		if (!err) response.write(buffer);
+		else {
+			if (err.code === 'ENOENT') response.writeHead(404, 'File not found');
+		}
 
 		response.end();
 	});
