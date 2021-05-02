efe5318a24094c55d3b4108ee92a80d9c18178c1
Dimitri-chln <dimitrichaline4@gmail.com>
2021-05-02 11:36:57 +0200

    Leaderboard complete!

---
+++ public/resources/js/leaderboard.mjs
@@ -0,0 +1,54 @@
+import { getCookie } from '../../modules/cookie.mjs';
+
+fetch(`api/leaderboard?token=${getCookie('token')}`, {
+    method: 'GET'
+})
+    .then(async res => {
+        const leaderboard = await res.json().catch(() => {});
+		if (!leaderboard.length) return;
+
+        const ol = document.getElementById('leaderboard');
+
+        for (let user of leaderboard) {
+            let li = document.createElement('li');
+
+            let divMember = document.createElement('div');
+            divMember.className = 'member';
+            let memberRank = document.createElement('span');
+                memberRank.innerHTML = user.rank;
+                memberRank.style.width = `${0.75 * user.rank.toString().length}em`;
+            let memberAvatar = document.createElement('img');
+                memberAvatar.src = user.avatar;
+                memberAvatar.alt = 'Avatar';
+            let memberName = document.createElement('p');
+                memberName.innerHTML = user.username;
+            divMember.appendChild(memberRank);
+            divMember.appendChild(memberAvatar);
+            divMember.appendChild(memberName);
+
+            let divStats = document.createElement('div');
+            divStats.className = 'member-stats';
+            let statsXP = document.createElement('p');
+                statsXP.innerHTML = formatXP(user.totalXP);
+            let statsLevel = document.createElement('p');
+                statsLevel.innerHTML = `Niveau ${user.level}`;
+            divStats.appendChild(statsXP);
+            divStats.appendChild(statsLevel);
+
+            li.appendChild(divMember);
+            li.appendChild(divStats);
+
+            ol.appendChild(li);
+        }
+
+        function formatXP(xp) {
+            const suffixes = [ '', 'k', 'm', 'g' ];
+
+            while (xp > 1000) {
+                suffixes.shift();
+                xp = xp / 1000;
+            }
+
+            return `${(Math.round(xp * 10) / 10) + suffixes[0]} XP`;
+        }
+    });
---
+++ routes/api/leaderboard/route.js
@@ -0,0 +1,72 @@
+const { IncomingMessage, ServerResponse } = require('http');
+const { URL } = require('url');
+const Pg = require('pg');
+const Discord = require('discord.js');
+const Fs = require('fs').promises;
+const Axios = require('axios').default;
+
+const route = {
+	name: 'leaderboard',
+	/**
+	 * @param {URL} url 
+	 * @param {IncomingMessage} request 
+	 * @param {ServerResponse} response 
+	 * @param {Discord.Client} discord 
+	 * @param {Pg.Client} pg 
+	 * @param {string} token
+	 */
+	run: async (url, request, response, discord, pg, token) => {
+		if (request.method.toUpperCase() !== 'GET' || !url.searchParams.has('token')) {
+			response.writeHead(400, { 'Content-Type': 'application/json' });
+			response.write(JSON.stringify({
+				status: 400,
+				message: 'Bad Request'
+			}));
+			return response.end();
+		}
+
+		const { 'rows': tokens } = await pg.query(`SELECT discord_user_id FROM web_clients WHERE '${url.searchParams.get('token')}' = ANY (mayze_tokens)`);
+		if (!tokens.length) {
+			response.writeHead(400, { 'Content-Type': 'application/json' });
+			response.write(JSON.stringify({
+				status: 401,
+				message: 'Not Connected'
+			}));
+			return response.end();
+		}
+
+		const guild = discord.guilds.cache.get('689164798264606784');
+		const members = guild.members.cache.filter(m => m.roles.cache.has('689169027922526235'));
+		const IDs = members.map(m => m.id);
+
+		pg.query(`SELECT * FROM levels WHERE user_id = ANY ('{ "${IDs.join('", "')}" }') ORDER BY xp DESC`)
+			.then(res => {
+				const data = res.rows.map((row, i) => {
+					const level = getLevel(row.xp);
+					return { rank: i + 1, username: members.get(row.user_id).user.username, avatar: members.get(row.user_id).user.avatarURL() || 'https://cdn.discordapp.com/embed/avatars/0.png', level: level[0], xp: level[1], totalXP: row.xp };
+				});
+
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				response.write(JSON.stringify(data));
+				response.end();
+			})
+			.catch(err => {
+				console.error(err);
+
+				response.writeHead(500, { 'Content-Type': 'application/json' });
+				response.write(JSON.stringify({
+					status: 500,
+					message: 'Internal Server Error'
+				}));
+				response.end();
+			});
+		
+		function getLevel(xp, lvl = 0) {
+			const xpPerLevel = 1000 + lvl * 250;
+			if (xp < xpPerLevel) return [ lvl, xp ];
+			return getLevel(xp - xpPerLevel, lvl + 1);
+		}
+	}
+};
+
+module.exports = route;
