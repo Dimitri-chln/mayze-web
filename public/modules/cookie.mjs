208ec0cca2183c34d37dcde7b9fd27dc91364283
Dimitri-chln <dimitrichaline4@gmail.com>
2021-05-22 19:04:30 +0200

    debug

--- public/modules/cookie.mjs
+++ public/modules/cookie.mjs
@@ -2,6 +2,7 @@ export function setCookie(cname, cvalue, exdays) {
 	let d = new Date();
 	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
 	document.cookie = `${cname}=${cvalue}; expires=${d.toUTCString()}; path=/; SameSite=strict`;
+	console.log(document.cookie);
 }
 
 export function getCookie(cname) {
