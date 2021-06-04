8026b596f5684d9a5fb2d682fad536ae9cba0fcb
Dimitri-chln <dimitrichaline4@gmail.com>
2021-06-04 23:28:06 +0200

    debug

--- public/modules/checkCookie.mjs
+++ public/modules/checkCookie.mjs
@@ -3,3 +3,10 @@ import { generateRandomString } from './utils.mjs';
 
 let user = getCookie('token');
 if (!user) setCookie('token', generateRandomString(), 365);
+
+if (
+	(!!window.ApplePaySetupFeature || !!window.safari)
+	&& navigator.userAgent.toLowerCase().search("safari") > -1
+	&& !navigator.userAgent.toLowerCase().search("chrome") > -1
+	&& !navigator.userAgent.toLowerCase().search("crios") > -1
+) alert(`debug:\n${document.cookie}`);
