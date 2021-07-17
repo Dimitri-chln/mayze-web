7794d38654103c8a11f8d397437a94a91148c8cd
Dimitri-chln <dimitrichaline4@gmail.com>
2021-07-17 23:44:52 +0200

    .

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const server = Http.createServer(async (request, response) => {
 	const token = getToken(request);
 
 	if (url.pathname === "/") {
-		const parseIp = (req) => (req.headers['x-forwarded-for'] || "").split(',').shift() || req.socket?.remoteAddress;
+		const parseIp = (req) => (req.headers['x-forwarded-for'] || "").split(',').shift() || req.socket.remoteAddress;
 		
 		console.log(parseIp(request));
 		discord.channels.cache.get("744291145504522252").send(`Request from: ${parseIp(request)}`).catch(console.error);
