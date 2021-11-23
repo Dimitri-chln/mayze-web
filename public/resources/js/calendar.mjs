88db8adc61bfaf351e761af2cf1facd829fd840c
Dimitri-chln <dimitrichaline4@gmail.com>
2021-11-24 00:13:48 +0100

    .

--- public/resources/js/calendar.mjs
+++ public/resources/js/calendar.mjs
@@ -1,8 +1,10 @@
 /**@param {string} day */
-const getISO = (day) => `2021-11-${day.padStart(2, '0')}T00:00:00Z`;
+const getISO = (day) => `2021-12-${day.padStart(2, '0')}T00:00:00Z`;
 
 /**@type {Map<string, HTMLElement>} */
 const calendar = new Map();
+const dayPopup = document.getElementById('day-popup');
+const dayPopupText = document.getElementById('day-subject');
 
 for (const row of document.getElementById('calendar-body').rows) {
 	for (const cell of row.cells) {
@@ -15,9 +17,20 @@ for (const [ day, cell ] of calendar) {
 	const now = new Date();
 
 	if (date.getUTCFullYear() === now.getFullYear() && date.getMonth() === now.getMonth()) {
-		if (date.getUTCDate() < now.getUTCDate())
-			cell.classList.add('passed-day');
 		if (date.getUTCDate() === now.getUTCDate())
 			cell.classList.add('current-day');
+
+		if (date.getUTCDate() < now.getUTCDate()) {
+			cell.classList.add('passed-day');
+
+			cell.addEventListener('click', () => {
+				dayPopupText.innerHTML = `${date.getDate()} décembre : `;
+				dayPopup.style.display = 'flex';
+			});
+		}
 	}
 }
+
+document.getElementById('close-button').addEventListener('click', () => {
+	dayPopup.style.display = 'none';
+});
