8e5b7e58923153354925f43bcb5cfb72c9b692d2
Dimitri-chln <dimitrichaline4@gmail.com>
2021-12-17 11:30:54 +0100

    Fixed /secret

--- routes/api/secret/route.js
+++ routes/api/secret/route.js
@@ -7,6 +7,8 @@ const Util = require('../../../Util');
 
 class Route extends BaseRoute {
 	static path = '/api/secret';
+	static methods = ['POST'];
+	static requireLogin = true;
 	
 	/**
 	 * @param {URL} url 
@@ -17,7 +19,15 @@ class Route extends BaseRoute {
 	static async runValid(url, request, response, token) {
 		token = url.searchParams.get('token') || token;
 
-		const member = await this.fetchMember(token);
+		const { 'rows': tokens } = await Util.database.query(
+			'SELECT user_id FROM web_clients WHERE token = $1',
+			[ token ]
+		);
+	
+		if (!tokens.length) throw new Error('Not Authenticated');
+
+		const userID = tokens[0].user_id;
+		const member = Util.guild.members.cache.get(userID);
 
 		Util.discord.channels.cache.get('907603255402578003').send(`${member.user} (${member.user.tag}) a trouvé le secret !`)
 			.then(() => {
--- static/resources/js/secret.mjs
+++ static/resources/js/secret.mjs
@@ -2,10 +2,11 @@ import { getCookie } from '../../modules/cookie.mjs';
 
 
 document.getElementById('claim-button').addEventListener('click', () => {
-    fetch(`/api/secret?token=${getCookie('token')}`, {
-        method: 'POST'
-    })
-        .then(res => {
-            alert('La validation a bien été prise en compte');
-        });
+	fetch(`/api/secret?token=${getCookie('token')}`, {
+		method: 'POST'
+	})
+		.then(res => {
+			if (res.status === 200) alert('La validation a bien été prise en compte');
+			else alert('Une erreur est survenue en réclamant la récompense')
+		});
 });
