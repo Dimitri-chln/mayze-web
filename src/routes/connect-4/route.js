6dba90a779993b834465c457f0b138a0b2a68f23
Dimitri-chln <dimitrichaline4@gmail.com>
2022-03-02 17:58:12 +0100

    .

--- src/routes/connect-4/route.js
+++ src/routes/connect-4/route.js
@@ -3,7 +3,6 @@ const BaseRoute = require('../../BaseRoute');
 class Route extends BaseRoute {
 	static path = '/connect-4';
 	static requireLogin = true;
-	static requireMember = true;
 }
 
 module.exports = Route;
