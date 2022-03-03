71ec837744b987b9ed1e134f77114ed360856c16
Dimitri-chln <dimitrichaline4@gmail.com>
2022-03-03 17:39:29 +0100

    .

---
+++ connect-4/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	bool weak = false;
+	bool analyze = false;
+
+	std::string line;
+	std::cin >> line;
+
+	std::cout << line;
+	std::cout << " " << 10 << std::endl;
+}
