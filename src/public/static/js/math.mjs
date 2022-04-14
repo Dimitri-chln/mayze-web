44659e08953aad4b99d4e9a2dd9dfab41af56cb9
Dimitri-chln <dimitrichaline4@gmail.com>
2022-04-14 19:50:15 +0200

    [New] Edit button

--- src/public/static/js/math.mjs
+++ src/public/static/js/math.mjs
@@ -1,17 +1,27 @@
 import katex from 'https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.mjs';
 
-const inputElement = document.getElementById('katex-input-field');
+const inputElement = document.getElementById('katex-input');
 const renderDiv = document.getElementById('katex-render');
+const editButton = document.getElementById('edit-button');
 const prefilledData = new URLSearchParams(location.search).get('prefill');
 
 if (prefilledData) {
+	editButton.style.display = 'block';
 	inputElement.value = atob(prefilledData);
-	updateRender();
+	renderLaTeX();
+} else {
+	inputElement.style.display = 'block';
 }
 
-inputElement.addEventListener('input', updateRender);
+inputElement.addEventListener('input', renderLaTeX);
 
-function updateRender() {
+editButton.addEventListener('click', () => {
+	editButton.style.display = 'none';
+	inputElement.style.display = 'block';
+	renderLaTeX();
+});
+
+function renderLaTeX() {
 	inputElement.style.height = '2em';
 	inputElement.style.height = `${inputElement.scrollHeight}px`;
 
@@ -20,5 +30,9 @@ function updateRender() {
 		throwOnError: false,
 	});
 
-	history.replaceState(null, null, `${location.pathname}?prefill=${btoa(inputElement.value)}`);
+	history.replaceState(
+		null,
+		null,
+		inputElement.value ? `${location.pathname}?prefill=${btoa(inputElement.value)}` : location.pathname,
+	);
 }
